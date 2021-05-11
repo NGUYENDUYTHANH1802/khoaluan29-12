db76f965a58be65a8cb947257ef492464f956622
=NGUYENDUYTHANH1802 <=nguyenduythanh77777@gmail.com>
2021-05-11 17:26:16 +0700

    edit img

--- app/Http/Controllers/Admin/ProductAdminController.php
+++ app/Http/Controllers/Admin/ProductAdminController.php
@@ -22,31 +22,6 @@ class ProductAdminController extends Controller
     	
     }
 
-  //    public function index()
-  //   {
-	 // 	return view('admin.pages.product.add');
-  //   }
-
- 	// public function create(Request $request)
-  //   {
-  //   	$blog = new bai_viet();
-
-  //      	$blog->mota = $request->mota;
-  //   	$blog->tieude = $request->tieude;
-  //   	$blog->id_nguoi_dung =2;
-  //   	$blog->noidung = $request->noidung;
-    	
-  //       if ($request->hasFile('hinhanh')) {
-  //           $file = $request->file('hinhanh');
-            
-  //           $imageName = time().$request->hinhanh->getClientOriginalName();
-  //           $request->hinhanh->move(public_path('User/img/blog/'), $imageName);
-  //           $blog->hinhanh = $imageName;
-  //       }
-  //       $blog->save();
-  //       return redirect('admin/list-blog');
-  //       // return redirect('blog.html');
-  //   }
     public function getXoa($id){
         
         $product = san_pham::find($id);
